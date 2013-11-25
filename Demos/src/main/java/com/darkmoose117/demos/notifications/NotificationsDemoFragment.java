8e9da237d3e1bbcc9f4c4e9b96460b382e3dc8b3
Joshua Lamson <jlamson@mymail.mines.edu>
2013-11-25 15:38:09 -0700

    Added button stub for simple notification

--- Demos/src/main/java/com/darkmoose117/demos/notifications/NotificationsDemoFragment.java
+++ Demos/src/main/java/com/darkmoose117/demos/notifications/NotificationsDemoFragment.java
@@ -5,19 +5,38 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
+import android.widget.Button;
+import android.widget.Toast;
 
-import com.darkmoose117.demos.R;
 import com.darkmoose117.demos.Constants;
+import com.darkmoose117.demos.R;
 
 /**
  * Created by Joshua Lamson on 11/25/13.
  */
-public class NotificationsDemoFragment extends Fragment implements Constants {
+public class NotificationsDemoFragment extends Fragment implements Constants, View.OnClickListener {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.notifications_demo_fragment, container, false);
-        ((TextView) view).setText(NOTIFICATIONS_DEMO_TITLE);
+
+        Button simpleNotificationButton = (Button) view.findViewById(R.id.simple_notification_button);
+        simpleNotificationButton.setOnClickListener(this);
+
         return view;
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.simple_notification_button:
+                showSimpleNotification();
+                break;
+            default:
+                Toast.makeText(getActivity(), "not implemeneted yet", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private void showSimpleNotification() {
+        Toast.makeText(getActivity(), "show simple notification", Toast.LENGTH_SHORT).show();
     }
 }
