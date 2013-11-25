ea2d0934dc77f3b1579d40eaaa5f0a36a8e61ade
Joshua Lamson <jlamson@mymail.mines.edu>
2013-11-25 16:19:23 -0700

    Show simple text only notification with no intent attached to it.

--- Demos/src/main/java/com/darkmoose117/demos/notifications/NotificationsDemoFragment.java
+++ Demos/src/main/java/com/darkmoose117/demos/notifications/NotificationsDemoFragment.java
@@ -1,7 +1,11 @@
 package com.darkmoose117.demos.notifications;
 
+import android.app.Activity;
+import android.app.NotificationManager;
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.NotificationCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -15,6 +19,11 @@ import com.darkmoose117.demos.R;
  * Created by Joshua Lamson on 11/25/13.
  */
 public class NotificationsDemoFragment extends Fragment implements Constants, View.OnClickListener {
+
+    private static final int SIMPLE_NOTIFICATION_ID = 0;
+
+    private NotificationManager mNotificationManager;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.notifications_demo_fragment, container, false);
@@ -23,6 +32,14 @@ public class NotificationsDemoFragment extends Fragment implements Constants, Vi
         simpleNotificationButton.setOnClickListener(this);
 
         return view;
+    }
+
+    @Override
+    public void onAttach(Activity activity) {
+        super.onAttach(activity);
+
+        mNotificationManager = (NotificationManager) activity.getSystemService(
+                Context.NOTIFICATION_SERVICE);
     }
 
     @Override
@@ -37,6 +54,11 @@ public class NotificationsDemoFragment extends Fragment implements Constants, Vi
     }
 
     private void showSimpleNotification() {
-        Toast.makeText(getActivity(), "show simple notification", Toast.LENGTH_SHORT).show();
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(getActivity())
+                .setSmallIcon(R.drawable.ic_stat_test)
+                .setContentTitle("Sup yo")
+                .setContentText("This is a simple notification");
+
+        mNotificationManager.notify(SIMPLE_NOTIFICATION_ID, builder.build());
     }
 }
