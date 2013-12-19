dbbfc2baed8efae905711dc044edc61a636acd2f
Joshua Lamson <jlamson@mymail.mines.edu>
2013-12-19 15:08:54 -0700

    Added Layouts for from and preview texts.

--- Demos/src/main/java/com/darkmoose117/demos/customdrawing/EmailListItem.java
+++ Demos/src/main/java/com/darkmoose117/demos/customdrawing/EmailListItem.java
@@ -1,11 +1,16 @@
 package com.darkmoose117.demos.customdrawing;
 
+import android.app.Notification;
 import android.content.Context;
 import android.content.res.Resources;
 import android.graphics.Canvas;
 import android.graphics.Paint;
 import android.graphics.Typeface;
-import android.text.TextPaint;
+import android.text.*;
+import android.text.Layout.*;
+import android.text.style.StyleSpan;
+import android.text.style.TextAppearanceSpan;
+import android.text.style.TypefaceSpan;
 import android.util.AttributeSet;
 import android.view.View;
 import android.view.ViewGroup;
@@ -23,9 +28,13 @@ public class EmailListItem extends View {
 
     private Email mEmail;
 
-    private TextPaint mTextPaint;
     private Paint mIconPaint;
+    private TextPaint mFromPaint;
+    private TextPaint mPreviewPaint;
+    private StaticLayout mFromLayout;
+    private StaticLayout mPreviewLayout;
 
+    private boolean mHasNewData = false;
 
     public EmailListItem(Context context) {
         this(context, null);
@@ -44,6 +53,42 @@ public class EmailListItem extends View {
 
     public void setEmail(Email email) {
         mEmail = email;
+        mHasNewData = true;
+    }
+
+    @Override
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+        super.onSizeChanged(w, h, oldw, oldh);
+
+        if (mHasNewData || w != oldw || h != oldh) {
+            final int textWidth = w
+                    - 2 * sPadding - sIconSize // content and padding to left
+                    - sPadding; // content and padding on right
+
+            StringBuilder builder = new StringBuilder();
+            for (int i = 0; i < mEmail.people.size(); i++) {
+                builder.append(mEmail.people.get(i));
+                if (i >= 4) break; // don't want the list to get too long
+                if (i < mEmail.people.size() - 1) builder.append(", ");
+            }
+
+            mFromLayout = new StaticLayout(builder.toString(), mFromPaint, textWidth,
+                    Alignment.ALIGN_NORMAL, 1f, 1f, false);
+
+            builder = new StringBuilder();
+            builder.append(mEmail.subject);
+            builder.append(" \u2014 ");
+            builder.append(mEmail.body);
+
+            SpannableString preview = new SpannableString(builder.toString());
+            preview.setSpan(new StyleSpan(Typeface.BOLD), 0, mEmail.subject.length(), 0);
+
+            mPreviewLayout = new StaticLayout(preview, 0, preview.length(), mPreviewPaint,
+                    textWidth, Alignment.ALIGN_NORMAL, 1f, 1f, false,
+                    TextUtils.TruncateAt.END, textWidth);
+
+            mHasNewData = false;
+        }
     }
 
     @Override
@@ -64,7 +109,13 @@ public class EmailListItem extends View {
         super.onDraw(canvas);
 
         canvas.drawRect(sPadding, sPadding, sPadding + sIconSize, sPadding + sIconSize, mIconPaint);
-        canvas.drawText(mEmail.subject, 2 * sPadding + sIconSize, mTextPaint.getTextSize() + sPadding, mTextPaint);
+
+        canvas.save();
+        canvas.translate(2 * sPadding + sIconSize, sPadding);
+        mFromLayout.draw(canvas);
+        canvas.translate(0, mFromPaint.getTextSize() + 2);
+        mPreviewLayout.draw(canvas);
+        canvas.restore();
     }
 
     private void initDimens() {
@@ -77,12 +128,18 @@ public class EmailListItem extends View {
     private void initPaints() {
         Resources res = getResources();
 
+
         mIconPaint = new TextPaint();
         mIconPaint.setColor(res.getColor(R.color.purple));
 
-        mTextPaint = new TextPaint();
-        mTextPaint.setAntiAlias(true);
-        mTextPaint.setColor(res.getColor(R.color.subject_text_color));
-        mTextPaint.setTextSize(res.getDimensionPixelSize(R.dimen.subject_text_size));
+        mFromPaint = new TextPaint();
+        mFromPaint.setAntiAlias(true);
+        mFromPaint.setColor(res.getColor(R.color.subject_text_color));
+        mFromPaint.setTextSize(res.getDimensionPixelSize(R.dimen.from_text_size));
+
+        mPreviewPaint = new TextPaint();
+        mPreviewPaint.setAntiAlias(true);
+        mPreviewPaint.setColor(res.getColor(R.color.subject_text_color));
+        mPreviewPaint.setTextSize(res.getDimensionPixelSize(R.dimen.subject_text_size));
     }
 }
