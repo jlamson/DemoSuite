e4243c548477708325aaeb324af4d508f14667fd
Joshua Lamson <jlamson@mymail.mines.edu>
2013-12-19 17:50:10 -0700

    Added a short body to test when subject + message id less than two lines.

--- Demos/src/main/java/com/darkmoose117/demos/utils/EmailUtils.java
+++ Demos/src/main/java/com/darkmoose117/demos/utils/EmailUtils.java
@@ -12,7 +12,7 @@ import java.util.List;
  */
 public class EmailUtils {
 
-    private static String[][] sPeople = {
+    private static final String[][] sPeople = {
             {"Bob"},
             {"Jim", "Alice"},
             {"Dude", "Sam", "Doug", "Jamie", "Anj"},
@@ -20,7 +20,7 @@ public class EmailUtils {
             {"Sam"},
     };
 
-    private static String[] sSubjects = {
+    private static final String[] sSubjects = {
             "Sup!",
             "Time off",
             "[project] Name of Branch",
@@ -28,13 +28,14 @@ public class EmailUtils {
             "Spam Spam Spam",
     };
 
-    private static String sBody = "Normally, both your asses would be dead as fucking fried " +
+    private static final String sLongBody  = "Normally, both your asses would be dead as fucking fried " +
             "chicken, but you happen to pull this shit while I'm in a transitional period so I " +
             "don't wanna kill you, I wanna help you. But I can't give you this case, it don't " +
             "belong to me. Besides, I've already been through too much shit this morning over " +
-            "this case to hand it over to your dumb ass.\n";
+            "this case to hand it over to your dumb ass.";
+    private static String sShortBody = "How're you?";
 
-    private static int[] sNumberOfReplies = {
+    private static final int[] sNumberOfReplies = {
             1,
             3,
             17,
@@ -42,11 +43,11 @@ public class EmailUtils {
             1
     };
 
-    private static byte[] sIsFavorites = {0, 1, 0, 0, 1};
+    private static final byte[] sIsFavorites = {0, 1, 0, 0, 1};
 
-    private static byte[] sHasAttachments = {1, 0, 1, 0, 1};
-
-    private static long[] sTimes = {
+    private static final byte[] sHasAttachments = {1, 0, 1, 0, 1};
+    
+    private static final long[] sTimes = {
             System.currentTimeMillis() - 5  * DateUtils.MINUTE_IN_MILLIS,
             System.currentTimeMillis() - 20 * DateUtils.MINUTE_IN_MILLIS,
             System.currentTimeMillis() - 45 * DateUtils.MINUTE_IN_MILLIS,
@@ -60,16 +61,16 @@ public class EmailUtils {
     };
 
     private static final Email[] EMAILS = {
-        new Email(Arrays.asList(sPeople[0]), sSubjects[0], sBody, sNumberOfReplies[0], sIsFavorites[0] == 1, sHasAttachments[0] == 1, sTimes[0]),
-        new Email(Arrays.asList(sPeople[1]), sSubjects[1], sBody, sNumberOfReplies[1], sIsFavorites[1] == 1, sHasAttachments[1] == 1, sTimes[1]),
-        new Email(Arrays.asList(sPeople[2]), sSubjects[2], sBody, sNumberOfReplies[2], sIsFavorites[2] == 1, sHasAttachments[2] == 1, sTimes[2]),
-        new Email(Arrays.asList(sPeople[3]), sSubjects[3], sBody, sNumberOfReplies[3], sIsFavorites[3] == 1, sHasAttachments[3] == 1, sTimes[3]),
-        new Email(Arrays.asList(sPeople[4]), sSubjects[4], sBody, sNumberOfReplies[4], sIsFavorites[4] == 1, sHasAttachments[4] == 1, sTimes[4]),
-        new Email(Arrays.asList(sPeople[0]), sSubjects[0], sBody, sNumberOfReplies[0], sIsFavorites[0] == 1, sHasAttachments[0] == 1, sTimes[5]),
-        new Email(Arrays.asList(sPeople[1]), sSubjects[1], sBody, sNumberOfReplies[1], sIsFavorites[1] == 1, sHasAttachments[1] == 1, sTimes[6]),
-        new Email(Arrays.asList(sPeople[2]), sSubjects[2], sBody, sNumberOfReplies[2], sIsFavorites[2] == 1, sHasAttachments[2] == 1, sTimes[7]),
-        new Email(Arrays.asList(sPeople[3]), sSubjects[3], sBody, sNumberOfReplies[3], sIsFavorites[3] == 1, sHasAttachments[3] == 1, sTimes[8]),
-        new Email(Arrays.asList(sPeople[4]), sSubjects[4], sBody, sNumberOfReplies[4], sIsFavorites[4] == 1, sHasAttachments[4] == 1, sTimes[9])
+        new Email(Arrays.asList(sPeople[0]), sSubjects[0], sLongBody , sNumberOfReplies[0], sIsFavorites[0] == 1, sHasAttachments[0] == 1, sTimes[0]),
+        new Email(Arrays.asList(sPeople[1]), sSubjects[1], sShortBody, sNumberOfReplies[1], sIsFavorites[1] == 1, sHasAttachments[1] == 1, sTimes[1]),
+        new Email(Arrays.asList(sPeople[2]), sSubjects[2], sLongBody , sNumberOfReplies[2], sIsFavorites[2] == 1, sHasAttachments[2] == 1, sTimes[2]),
+        new Email(Arrays.asList(sPeople[3]), sSubjects[3], sShortBody, sNumberOfReplies[3], sIsFavorites[3] == 1, sHasAttachments[3] == 1, sTimes[3]),
+        new Email(Arrays.asList(sPeople[4]), sSubjects[4], sLongBody , sNumberOfReplies[4], sIsFavorites[4] == 1, sHasAttachments[4] == 1, sTimes[4]),
+        new Email(Arrays.asList(sPeople[0]), sSubjects[0], sShortBody, sNumberOfReplies[0], sIsFavorites[0] == 1, sHasAttachments[0] == 1, sTimes[5]),
+        new Email(Arrays.asList(sPeople[1]), sSubjects[1], sLongBody , sNumberOfReplies[1], sIsFavorites[1] == 1, sHasAttachments[1] == 1, sTimes[6]),
+        new Email(Arrays.asList(sPeople[2]), sSubjects[2], sShortBody, sNumberOfReplies[2], sIsFavorites[2] == 1, sHasAttachments[2] == 1, sTimes[7]),
+        new Email(Arrays.asList(sPeople[3]), sSubjects[3], sLongBody , sNumberOfReplies[3], sIsFavorites[3] == 1, sHasAttachments[3] == 1, sTimes[8]),
+        new Email(Arrays.asList(sPeople[4]), sSubjects[4], sShortBody, sNumberOfReplies[4], sIsFavorites[4] == 1, sHasAttachments[4] == 1, sTimes[9])
     };
 
     public static List<Email> getDemoEmailList() {
